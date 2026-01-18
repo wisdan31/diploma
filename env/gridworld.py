810a02efd41ebad92eb7b9f4965d584933c39077
wisdan31 <forspamletters@protonmail.com>
2026-01-18 03:00:46 +0200

    Refactor GridEnv initialization and update main execution flow to include grid, start, and goal parameters

--- env/gridworld.py
+++ env/gridworld.py
@@ -1,13 +1,21 @@
 class GridEnv:
-    def __init__(self, size):
+    def __init__(self, grid, start, goal, size):
         self.size = size
-        self.agent_pos = [0, 0]
+        self.grid = grid
+        self.start = start
+        self.goal = goal
+        self.agent_pos = list(start)
 
     def observe(self):
         return tuple(self.agent_pos), self.size
+    
+    def is_terminal(self):
+        return tuple(self.agent_pos) == self.goal
 
     def step(self, action):
         if action == "DOWN":
             self.agent_pos[0] += 1
         elif action == "RIGHT":
             self.agent_pos[1] += 1
+
+        return self.observe()
--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from env.gridworld import GridEnv
 from agents import Agent 
 from policies import SweepPolicy   
+import numpy as np
 
-env = GridEnv(10)
+grid = np.zeros((10, 10), dtype=int)
+
+
+
+env = GridEnv(grid, (0, 0), (9, 9), 10)
 agent = Agent(SweepPolicy())
 
-while env.agent_pos != [9, 9]:
-    obs = env.observe()
-    action = agent.act(obs)
-    env.step(action)
+def run_experiment(agent, env):
+    while not env.is_terminal():
+        observation = env.observe()
+        action = agent.act(observation)
+        env.step(action)
 
-print("Finished at", env.agent_pos)
+    print("Finished at", env.agent_pos)
+
+
+run_experiment(agent, env)
