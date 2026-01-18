76d617ccd9e30b90e85fef11771b973ae55e8be5
wisdan31 <forspamletters@protonmail.com>
2026-01-19 01:28:50 +0200

    add log of agent path

--- simulations.py
+++ simulations.py
@@ -2,6 +2,7 @@ class Simulation:
     def __init__(self, agent, env):
         self.agent = agent
         self.env = env
+        self.visited_nodes = []
 
     def run():
         return None
@@ -10,7 +11,10 @@ class SimpleSimulation(Simulation):
     def run(self):
         while not self.env.is_terminal():
             observation = self.env.observe()
+            current_node = observation[0]
+            self.visited_nodes.append(current_node)
             action = self.agent.act(observation)
             self.env.step(action)
 
         print("Finished at", self.env.agent_pos)
+        print("Path of agent:", self.visited_nodes)
